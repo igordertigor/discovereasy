318b54b7400ae61f6ddc02ed1569d0f47bc0c7f2
Ingo Fruend <ingo@tapedeq.com>
2024-01-15 11:03:22 +0100

    ci: prepared publish script

--- publish.sh
+++ publish.sh
@@ -2,38 +2,22 @@
 
 set -xe
 
-BACKUP_SETUP=$(mktemp)
-NEW_SETUP=$(mktemp)
-
-cp setup.cfg $BACKUP_SETUP
-
-function cleanup() {
-  mv $BACKUP_SETUP setup.cfg
-  rm $NEW_SETUP
+function cleanup () {
+  echo No cleanup needed
 }
 
-track cleanup ERR
+trap cleanup ERR
 
-# bump version
-sed -r 's/version = ([0-9]+)/echo version = $((\1+1))"/e' setup.cfg > $NEW_SETUP
-mv $NEW_SETUP setup.cfg
-
-VERSION=$(awk '/version = [0-9]+/ { print $3 }' setup.cfg)
+VERSION=$(git tag | tail -n 1 | sed -r 's/v//')
 
 SOURCEDIST=discovereasy-${VERSION}.tar.gz
 BINDIST=csvmodel-${VERSION}-py3-name-any.whl
 
+echo $VERSION
 
 cram README.md
-
 
 python -m build
 
 twine check dist/$SOURCEDIST dist/$BINDIST
 twine upload --verbose dist/$SOURCEDIST dist/$BINDIST
-
-echo "****************************************************"
-echo "* Now commit setup.cfg and tag the commit as v${VERSION}"
-echo "****************************************************"
-
-rm $BACKUP_SETUP
